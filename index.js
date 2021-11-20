39cd6418d78ba3ebd39fae2f1e46c5bf3834cd53
unknown <kamalsabdulrashid@gmail.com>
2021-11-20 06:38:59 +0100

    main

---
+++ index.js
@@ -0,0 +1,11 @@
+function Area() {
+    let side1 = parseInt(document.getElementById("side1").value);
+    let side2 = parseInt(document.getElementById("side2").value);
+    let side3 = parseInt(document.getElementById("side3").value);
+
+    let s = (side1 + side2 + side3) / 2;
+
+    let Area = Math.sqrt(s * ((s - side1) * (s - side2) * (s - side3)));
+
+    document.getElementById("display").innerHTML = Area
+}
